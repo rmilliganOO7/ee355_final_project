9652ef4e59aedf054a64e2715f17222ae69ef207
brysonngo <btngo@usc.edu>
2026-04-22 13:01:01 -0700

    Add files via upload

---
+++ part2/person.cpp
@@ -0,0 +1,149 @@
+#include "person.h"
+
+Person::Person(){
+    // I'm already done! 
+    set_person();
+}
+
+
+Person::~Person(){
+    delete birthdate;
+    // TODO: complete the method!
+    delete email;
+    delete phone;
+}
+
+
+Person::Person(string f_name, string l_name, string b_date, string email, string phone){
+    // TODO: Complete this method!
+    // phone and email strings are in full version
+    this -> f_name = f_name;
+    this -> l_name = l_name;
+    
+    birthdate = new Date(b_date);
+    
+    //parse email string
+    string email_type = email.substr(1, email.find(')')- 1);
+    string email_addr = email.substr(email.find(')') + 2);
+    this -> email = new Email(email_type, email_addr);
+    
+    string phone_type = phone.substr(1, phone.find(')') - 1);
+    string phone_num = phone.substr(phone.find(')')+2);
+    this->phone = new Phone(phone_type, phone_num);
+    
+    next = NULL;
+    prev = NULL;
+}
+
+
+Person::Person(string filename){
+    set_person(filename);
+}
+
+
+void Person::set_person(){
+    // prompts for the information of the user from the terminal
+    // first/last name can have spaces!
+    // date format must be "M/D/YYYY"
+    // We are sure user enters info in correct format.
+    // TODO: complete this method!
+    
+    string temp;
+    string type;
+
+    cout << "First Name: ";
+    // pay attention to how we read first name, as it can have spaces!
+    std::getline(std::cin,f_name);
+
+	cout << "Last Name: ";
+    std::getline(std::cin,l_name);
+
+    cout << "Birthdate (M/D/YYYY): ";
+    std::getline(std::cin,temp);
+    // pay attention to how we passed argument to the constructor of a new object created dynamically using new command
+    birthdate = new Date(temp); 
+
+    cout << "Type of email address: ";
+    // code here
+    std::getline(std::cin, type);
+    cout << "Email address: ";
+    // code here
+    std::getline(std::cin, temp);
+    email = new Email(type, temp);
+
+    cout << "Type of phone number: ";
+    // code here
+    std::getline(std::cin, type);
+    cout << "Phone number: ";
+    // code here
+    std::getline(std::cin, temp);
+    phone = new Phone(type, temp);
+    // code here
+    next = NULL;
+    prev = NULL;
+}
+
+
+void Person::set_person(string filename){
+    // reads a Person from a file
+    // Look at person_template files as examples.     
+    // Phone number in files can have '-' or not.
+    // TODO: Complete this method!
+    ifstream infile(filename.c_str());
+    
+    string temp, type;
+    
+    getline(infile, f_name);
+    getline(infile, l_name);
+    getline(infile, temp);
+    birthdate = new Date(temp);
+    
+    getline(infile, temp);
+    type = temp.substr(1, temp.find(')')-1);
+    string num = temp.substr(temp.find(')')+2);
+    phone = new Phone(type,num);
+    
+    getline(infile, temp);
+    type = temp.substr(1, temp.find(')')-1);
+    string addr = temp.substr(temp.find(')')+2);
+    email = new Email(type, addr);
+    
+    next = NULL;
+    prev = NULL;
+}
+
+
+bool Person::operator==(const Person& rhs){
+    // TODO: Complete this method!
+    // Note: you should check first name, last name and birthday between two persons
+    // refer to bool Date::operator==(const Date& rhs)
+    return (f_name == rhs.f_name && l_name == rhs.l_name && *birthdate == *rhs.birthdate);
+}
+
+bool Person::operator!=(const Person& rhs){ 
+    // TODO: Complete this method!
+    return !(*this == rhs);
+}
+
+
+void Person::print_person(){
+    // Already implemented for you! Do not change!
+	cout << l_name <<", " << f_name << endl;
+	birthdate->print_date("Month D, YYYY");
+    phone->print();
+    email->print();
+}
+
+// part 2 (bryson)
+void Person::makeFriend(Person* newFriend){
+    if (newFriend == NULL || newFriend == this)
+        return;
+
+    // prevent duplicates
+    for (int i = 0; i < (int)myfriends.size(); i++){
+        if (myfriends[i] == newFriend)
+            return;
+    }
+
+    myfriends.push_back(newFriend);
+}
---
+++ part2/person.h
@@ -0,0 +1,40 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include "date.h"
+#include "contact.h"
+#include "fstream"
+#include <vector> //including vector
+class Person{
+    friend class Network;
+
+private:
+	string f_name;
+	string l_name;
+	Date *birthdate;
+    Email *email;
+    Phone *phone;
+    
+    vector<Person*> myfriends; // part 2 add friend vector (bryson)
+
+    // the following to attributes are used in the linked list.
+    Person* next;
+    Person* prev;
+
+public: 
+    Person();
+    ~Person();
+    Person(string filename);
+    Person(string f_name, string l_name, string b_date, string email, string phone);
+	void print_person();
+	void set_person();
+	void set_person(string filename);
+
+    void makeFriend(Person* newFriend); // part2 add makeFriend function (bryson)
+
+    bool operator==(const Person& rhs);
+    bool operator!=(const Person& rhs);
+};
+
+
+#endif
